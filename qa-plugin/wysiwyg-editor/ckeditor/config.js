f0713f1d6db684dd05e4cb1b4f7d33015fae2448
webaib <webaib@gmail.com>
2016-02-24 16:14:21 +0100

    Test

--- qa-plugin/wysiwyg-editor/ckeditor/config.js
+++ qa-plugin/wysiwyg-editor/ckeditor/config.js
@@ -1,48 +1,52 @@
 /**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights
+ *          reserved. For licensing, see LICENSE.md or
+ *          http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-    // Define changes to default configuration here. For complete reference see:
-    // http://docs.ckeditor.com/#!/api/CKEDITOR.config
+CKEDITOR.editorConfig = function(config) {
+	// Define changes to default configuration here. For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-    // NOTE: if you make changes to this file, make sure that you do not overwrite it when upgrading Q2A!
+	// NOTE: if you make changes to this file, make sure that you do not
+	// overwrite it when upgrading Q2A!
 
-    // The toolbar arrangement, two rows of buttons
-    config.toolbar = [
-        {name: 'insert', items: ['Image']}
-    ];
+	// The toolbar arrangement, two rows of buttons
+	config.toolbar = [ {
+		name : 'insert',
+		items : [ 'Image' ]
+	} ];
 
-    // Set the most common block elements
-    config.format_tags = 'p;h1;h2;h3;pre';
-    config.entities = false;
+	// Set the most common block elements
+	config.format_tags = 'p;h1;h2;h3;pre';
+	config.entities = false;
 
-    // Make dialogs simpler
-    config.removeDialogTabs = 'image:advanced;link:advanced;table:advanced';
+	// Make dialogs simpler
+	config.removeDialogTabs = 'image:advanced;link:advanced;table:advanced';
 
-    // Use native spell checking (note: Ctrl+right-click is required for native context menu)
-    config.disableNativeSpellChecker = false;
+	// Use native spell checking (note: Ctrl+right-click is required for native
+	// context menu)
+	config.disableNativeSpellChecker = false;
 
-    // Prevent blank paragraphs
-    config.fillEmptyBlocks = false;
+	// Prevent blank paragraphs
+	config.fillEmptyBlocks = false;
 };
 
-CKEDITOR.on('dialogDefinition', function( ev ) {
-    // Take the dialog window name and its definition from the event data.
-    var dialogName = ev.data.name;
-    var dialogDefinition = ev.data.definition;
+CKEDITOR.on('dialogDefinition', function(ev) {
+	// Take the dialog window name and its definition from the event data.
+	var dialogName = ev.data.name;
+	var dialogDefinition = ev.data.definition;
 
-    if ( dialogName == 'image' ) {
-    	dialogDefinition.width = 340;
-    	var infoTab = dialogDefinition.getContents('info');
-    	// infoTab.remove('htmlPreview');
-    	infoTab.remove( 'txtBorder' );
-        infoTab.remove( 'txtHSpace' );
-        infoTab.remove( 'txtVSpace' );
-        
-        dialogDefinition.onShow = function() {
-            this.selectPage( 'Upload' );
-        };
-    }
+	if (dialogName == 'image') {
+		dialogDefinition.width = 340;
+		var infoTab = dialogDefinition.getContents('info');
+		// infoTab.remove('htmlPreview');
+		// infoTab.remove( 'txtBorder' );
+		// infoTab.remove( 'txtHSpace' );
+		// infoTab.remove( 'txtVSpace' );
+
+		dialogDefinition.onShow = function() {
+			this.selectPage('Upload');
+		};
+	}
 });
