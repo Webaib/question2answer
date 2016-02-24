e5dc661664f03b285c81ca493c6b2513da58e96a
webaib <webaib@gmail.com>
2016-02-24 14:07:44 +0100

    Setup toolbar CKEditor

--- qa-plugin/wysiwyg-editor/ckeditor/config.js
+++ qa-plugin/wysiwyg-editor/ckeditor/config.js
@@ -4,36 +4,27 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+    // Define changes to default configuration here. For complete reference see:
+    // http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	// NOTE: if you make changes to this file, make sure that you do not overwrite it when upgrading Q2A!
+    // NOTE: if you make changes to this file, make sure that you do not overwrite it when upgrading Q2A!
 
-	// The toolbar arrangement, two rows of buttons
-	config.toolbar = [
-		{ name: 'basic', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript' ] },
-		{ name: 'color', items: [ 'TextColor', 'BGColor' ] },
-		{ name: 'align', items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
-		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
-		'/',
-		{ name: 'font', items: [ 'Font', 'FontSize', 'Format' ] },
-		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote' ] },
-		{ name: 'links', items: [ 'Link', 'Unlink' ] },
-		{ name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule', 'SpecialChar', 'Smiley' ] },
-		{ name: 'last', items: [ 'RemoveFormat', 'Maximize' ] }
-	];
+    // The toolbar arrangement, two rows of buttons
+    config.toolbar = [
+        {name: 'insert', items: ['Image']}
+    ];
 
-	// Set the most common block elements
-	config.format_tags = 'p;h1;h2;h3;pre';
-	config.entities = false;
+    // Set the most common block elements
+    config.format_tags = 'p;h1;h2;h3;pre';
+    config.entities = false;
 
-	// Make dialogs simpler
-	config.removeDialogTabs = 'image:advanced;link:advanced;table:advanced';
+    // Make dialogs simpler
+    config.removeDialogTabs = 'image:advanced;link:advanced;table:advanced';
 
-	// Use native spell checking (note: Ctrl+right-click is required for native context menu)
-	config.disableNativeSpellChecker = false;
+    // Use native spell checking (note: Ctrl+right-click is required for native context menu)
+    config.disableNativeSpellChecker = false;
 
-	// Prevent blank paragraphs
-	config.fillEmptyBlocks = false;
+    // Prevent blank paragraphs
+    config.fillEmptyBlocks = false;
 
 };
